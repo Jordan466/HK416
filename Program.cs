665b6915b19b84a9d6b67e02c568fbb7f298ad9f
Jordan466 <jordanw@inxsoftware.com>
2020-08-01 13:38:12 +0800

    bot

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace discordbot
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var client = new DiscordSocketClient();
+            client.Log += Log;
+
+            await client.LoginAsync(TokenType.Bot, args[0]);
+            await client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
