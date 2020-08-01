6fa5f5c3a92cf1130d157f9df958ae0238b14be6
Jordan466 <jordanw@inxsoftware.com>
2020-08-01 13:51:34 +0800

    ping pong

--- Program.cs
+++ Program.cs
@@ -9,9 +9,9 @@ namespace discordbot
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             var client = new DiscordSocketClient();
             client.Log += Log;
+            client.MessageReceived += Ping;
 
             await client.LoginAsync(TokenType.Bot, args[0]);
             await client.StartAsync();
@@ -22,6 +22,12 @@ namespace discordbot
         {
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
+        }
+
+        private async static Task Ping(SocketMessage msg)
+        {
+            if (msg.Content == "!ping")
+                await msg.Channel.SendMessageAsync("Pong!");
         }
     }
 }
